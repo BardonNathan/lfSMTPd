927b8a969a6ace7fb7b36dc8f4968a77c4efa6b0
BARDON Nathan <bardon.nathan@gmail.com>
2020-01-16 12:57:42 +0100

    Example to test server

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -1,4 +1,15 @@
+/* lfSMTPd - LazyFox SMTP daemon
+ * Author  : <nbardon@lazyfox.fr>
+ * Created : 16 jan 2020
+ */
+
+#include "Logger.h"
+#include "Server.h"
 
 int main(int argc, const char** argv) {
-
+	logln("Starting LazyFox SMTP daemon...");
+	
+	Server s;
+	s.start();
+	s.join();
 }
