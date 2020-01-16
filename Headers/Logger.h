6a3c52571acf91d520e4d5d8d0235c043eb28186
BARDON Nathan <bardon.nathan@gmail.com>
2020-01-16 10:36:51 +0100

    Handle logs via preprocessors

---
+++ Headers/Logger.h
@@ -0,0 +1,56 @@
+#ifndef INC_LF_LOGGER
+#define INC_LF_LOGGER
+
+/* Set VERBOSE preprocessor to enable Logging
+ * Set verbose value (integer) to define logging level.
+ *   - 1: Error
+ *   - 2: Warning
+ *   - 3: Info
+ *   - 4: Verbose 
+ * 
+ *  Define LOG_COLORIZED to enable bash colors
+ */
+#ifndef VERBOSE
+    #define VERBOSE 0
+#endif
+
+#ifdef VERBOSE
+    #include <iostream>
+    #define log(x) std::cout << x
+    #define logln(x) std::cout << x << std::endl
+
+    #ifdef LOG_COLORIZED 
+        #define COLOR_INFO  "\033[34m" <<
+        #define COLOR_WARN  "\033[33m" <<
+        #define COLOR_ERROR "\033[31m" <<
+        #define COLOR_RESET "\033[0m"  <<
+    #else
+        #define COLOR_WARN
+        #define COLOR_ERROR
+        #define COLOR_RESET
+    #endif
+
+
+    #if VERBOSE >= 3
+        #define logInfo(x) std::cout << COLOR_INFO "[Info] " << x << COLOR_RESET std::endl
+    #else
+        #define logInfo(x)
+    #endif
+
+    #if VERBOSE >= 2
+        #define logWarn(x) std::cout << COLOR_WARN "[Warn] " << x << COLOR_RESET std::endl
+    #else
+        #define logWarn(x)
+    #endif
+
+    #if VERBOSE >= 1
+        #define logError(x)std::cout << COLOR_ERROR "[Error] " << x << COLOR_RESET std::endl
+    #else
+        #define logError(x)
+    #endif
+
+#else
+    #define log(x)
+#endif
+
+#endif
