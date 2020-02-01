a54cd9cfb20b3c67f26c05b333d6ca4e954fb661
=BARDON Nathan <=bardon.nathan@gmail.com>
2020-02-01 23:33:08 +0100

    Moving the configuration parser to its own files

---
+++ Headers/Config.h
@@ -0,0 +1,31 @@
+#ifndef INCL_LF_CONFIG
+#define INCL_LF_CONFIG
+
+#include "Logger.h"
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <list>
+//#include <cstdlib>
+
+#define EXITCODE_ERR_OPNFILE -1
+#define EXITCODE_DEFAULT_CONF_CREATED -2
+
+/** Structure to store running configuration */
+struct {
+    char *hostname;
+    int port;
+    int timeout;
+    int maxMessageSize;
+
+    int motdDelay;
+    int motqDelay;
+    std::list<std::string> motd;
+    std::list<std::string> motq;
+} Config;
+
+int parseConfig(const char *filename);
+int createDefaultConfig(const char *filename);
+int initConfig(const char *filename);
+
+#endif
